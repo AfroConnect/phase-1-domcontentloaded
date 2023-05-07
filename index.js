bd87e11b931e6258ec60bb95d646f2fc9bdd094e
AfroConnect <mensahmedia23@gmail.com>
2023-05-07 05:33:18 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+function changeTextOnLoad() {
+    document.addEventListener('DOMContentLoaded', function() {
+      let pElement = document.querySelector('p');
+      if (pElement.textContent === "JavaScript is so cool. It lets me add text to my page programmatically.") {
+        pElement.textContent = "This is really cool!";
+      }
+    });
+  }
+  
+  changeTextOnLoad();
+
+
+
+
+
+
+ 
